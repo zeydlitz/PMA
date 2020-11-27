f367d1ecb1ffe9473a2256acae98dfda4cc00261
Max <maksimka.ivashkevich27@gmail.com>
2020-11-27 13:51:49 +0300

    gdq2

--- tonk/settings.py
+++ tonk/settings.py
@@ -107,8 +107,8 @@ df = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(df)
 DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
-GDAL_LIBRARY_PATH = os.getenv('GDAL_LIBRARY_PATH')
-GEOS_LIBRARY_PATH = os.getenv('GEOS_LIBRARY_PATH')
+GDAL_LIBRARY_PATH = os.environ.get('GDAL_LIBRARY_PATH')
+GEOS_LIBRARY_PATH = os.environ.get('GEOS_LIBRARY_PATH')
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
